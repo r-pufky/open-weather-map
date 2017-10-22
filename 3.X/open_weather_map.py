f6f36ac72b3d3233e0a7b4e506a3c87db8d0609e
r-pufky <r-pufky@users.noreply.github.com>
2017-10-22 10:20:41 -0700

    Fork for Python 2.X and 3.X support.
    Create initial fork for 2.X and 3.X Python support.

---
+++ 3.X/open_weather_map.py
@@ -0,0 +1,69 @@
+#
+# Python interface for OpenWeatherMap.
+#
+# This library provides a skeleton API interface for openweathermap.org.
+#
+# Details of API can be found here: http://openweathermap.org/api
+# Weather conditions enumeration: http://openweathermap.org/weather-conditions
+#
+
+import json
+import urllib.request
+
+
+class OpenWeatherMap(object):
+  """ OpenWeatherMap public (free) API interface.
+  
+  Attributes:
+    API_BASE: String base url used for API calls.
+  """
+  API_BASE = 'http://api.openweathermap.org/data/2.5/'
+
+  def __init__(self, api_key, units='imperial'):
+    """ Initialize OpenWeatherMap.
+
+    Args:
+      api_key: String API key to use for openweathermap.org.
+      units: String units to return results in. Default: imperial.
+          [metric, imperial, kelvin].
+    """
+    self.api_key = api_key
+    self.units = units
+    if units not in ['metric', 'imperial', 'kelvin']:
+      self.units = 'imperial'
+    
+  def _QueryApi(self, endpoint):
+    """ Queries a specific API endpoint and store in local cache.
+
+    Args:
+      endpoint: String endpoint to query.
+    """
+    query = '%s%s&units=%s&APPID=%s' % (self.API_BASE,
+                                        endpoint,
+                                        self.units,
+                                        self.api_key)
+    with urllib.request.urlopen(query) as data:
+      json_data = data.read()
+      return json.loads(json_data.decode('utf-8'))
+
+  def GetCurrentWeather(self, city=5809844):
+    """ Return dictionary with the current weather data for a given city.
+
+    Args:
+      cite: Integer city ID from http://bulk.openweathermap.org/sample/.
+
+    Returns:
+      Dictionary containing {'id': Integer weather type,
+                             'main': String main weather description,
+                             'description': String of weather details,
+                             'icon': String coded icon to use,
+                             'temp': Integer current temperature,
+                             'temp_min': Integer minimum temperature,
+                             'temp_max': Integer maximum temperature,
+                             'humidity': Integer current humidity}.
+    """
+    json_data = self._QueryApi('weather?id=%s' % city)
+    weather = json_data['weather'][0]
+    weather.update(json_data['main'])
+    weather.pop('pressure')
+    return weather
--- README.md
+++ README.md
@@ -7,6 +7,12 @@ implementation, and only features which are used have been implemented.
 
 Enumerated weather conditions can be found here: http://openweathermap.org/weather-conditions
 
+# Python 2.X and 3.X Support
+Compatible libraries for both 2.X and 3.X python are in respective directories
+Import/use the right directory for the respective version of Python you are
+using.
+
+
 # Usage
 
 ```python
